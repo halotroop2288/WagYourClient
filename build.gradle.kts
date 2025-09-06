7f0cfa4ee767b85f993c57d1d6941d75d75fb13c
Caroline Joy Bell <caroline@halotroop.com>
2025-09-06 11:15:25 -0700

    Update to Unimined 1.4.2
    Also updated PatchBase to 1.2.0 and Gradle wrapper to 9.0.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("xyz.wagyourtail.unimined") version "1.3.15"
-	id("xyz.wagyourtail.patchbase") version "1.1.0-SNAPSHOT"
+    id("xyz.wagyourtail.unimined") version "1.4.2"
+	id("xyz.wagyourtail.patchbase") version "1.2.0-SNAPSHOT"
     `maven-publish`
 }
 
